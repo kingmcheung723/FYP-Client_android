07278c51c12bf72e94d8bd5b02d983a2d02b6ca0
Tsz Wing Yim <natalietw.yim@gmail.com>
2015-04-02 17:44:13 +0800

    add a marker

--- PriceSharingApplication/app/src/main/java/com/example/tszwingyim/pricesharingapplication/SearchLocation.java
+++ PriceSharingApplication/app/src/main/java/com/example/tszwingyim/pricesharingapplication/SearchLocation.java
@@ -15,20 +15,28 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.Spinner;
 
-public class SearchLocation extends ActionBarActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class SearchLocation extends ActionBarActivity implements OnMapReadyCallback {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Hide the action bar
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
         setContentView(R.layout.activity_search_location);
+        //declare buttons
         Button recommendation = (Button) findViewById(R.id.button_recommend);
         Button category = (Button) findViewById(R.id.button_category);
         Button member = (Button) findViewById(R.id.button_member);
         Button barcode = (Button) findViewById(R.id.button_barcode);
         Button goods = (Button) findViewById(R.id.button_goods);
-
-
+      //  addMarker();
+        //set onclick listener for tabs
         member.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
@@ -69,33 +77,64 @@ public class SearchLocation extends ActionBarActivity {
 
             }
         });
-
-
+        //declare google map fragment
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        //configure the google map
+        GoogleMapOptions options = new GoogleMapOptions();
+        options.mapType(GoogleMap.MAP_TYPE_SATELLITE)
+                .compassEnabled(false)
+                .rotateGesturesEnabled(false)
+                .tiltGesturesEnabled(false);
+        // add spinner
         Spinner district = (Spinner) findViewById(R.id.spinner_searchdistrict);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.searchdistrict_array, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
+        // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
+        // Apply the adapter to the spinner
         district.setAdapter(adapter);
         // Get a reference to the AutoCompleteTextView in the layout
         AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.autoComplete_searchlocation);
-// Get the string array
+        // Get the string array
         String[] shoplocation = getResources().getStringArray(R.array.searcharea_array);
-// Create the adapter and set it to the AutoCompleteTextView
+        // Create the adapter and set it to the AutoCompleteTextView
         ArrayAdapter<String> adapter3 =
                 new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, shoplocation);
         textView.setAdapter(adapter3);
     }
 
+    /**
+     * Adds a marker to the map
+     */
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(22.192295, 114.130359))
+                .title("Hello world"));
+    }
 
+//    GoogleMap googleMap;
+//    private void addMarker(){
+//
+//        /** Make sure that the map has been initialised **/
+//        if(null != googleMap){
+//            googleMap.addMarker(new MarkerOptions()
+//                            .position(new LatLng(22.206321, 114.133534))
+//                            .title("Marker")
+//                            .draggable(true)
+//            );
+//        }
+//    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_search_location, menu);
         return true;
     }
+
+
 
         @Override
         public boolean onOptionsItemSelected (MenuItem item){
