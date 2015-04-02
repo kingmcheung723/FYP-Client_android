67c3fcd9f1eafd2577d3ea4b0ae6a8bde064f0f2
Tsz Wing Yim <natalietw.yim@gmail.com>
2015-04-02 17:09:07 +0800

    Googlemap

--- PriceSharingApplication/app/src/main/java/com/example/tszwingyim/pricesharingapplication/SearchLocation.java
+++ PriceSharingApplication/app/src/main/java/com/example/tszwingyim/pricesharingapplication/SearchLocation.java
@@ -14,15 +14,6 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.Toast;
-import com.google.android.gms.maps.UiSettings;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.*;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.SupportMapFragment;
 
 public class SearchLocation extends ActionBarActivity {
     public void onCreate(Bundle savedInstanceState) {
@@ -37,38 +28,7 @@ public class SearchLocation extends ActionBarActivity {
         Button barcode = (Button) findViewById(R.id.button_barcode);
         Button goods = (Button) findViewById(R.id.button_goods);
 
-        createMapView();
-     //  createStreetView();
-        addMarker();
-//
-//        GoogleMap googleMap; // Might be null if Google Play services APK is not available.
-//            setUpMapIfNeeded();
 
-//    googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-//            @Override
-//            public void onMapClick(LatLng latLng) {
-//
-//                /**
-//                 * Ensure the street view has been initialised correctly and
-//                 * pass it through the selected lat/long co-ordinates.
-//                 */
-//                if (m_StreetView != null) {
-//
-//                    /**
-//                     * Hide the map view to expose the street view.
-//                     */
-//                    Fragment mapView = getFragmentManager().findFragmentById(R.id.mapView);
-//                    getFragmentManager().beginTransaction().hide(mapView).commit();
-//
-//                    /** Passed the tapped location through to the Street View **/
-//                    m_StreetView.setPosition(latLng);
-//                }
-//            }
-//        });
-//        if (googleMap != null) {
-//            googleMap.setMyLocationEnabled(true);
-//            googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-//        }
         member.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
@@ -129,6 +89,7 @@ public class SearchLocation extends ActionBarActivity {
         textView.setAdapter(adapter3);
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -136,84 +97,8 @@ public class SearchLocation extends ActionBarActivity {
         return true;
     }
 
-    /**
-     * Initialises the mapview
-     */
-    GoogleMap googleMap;
-
-    private void createMapView() {
-        /**
-         * Catch the null pointer exception that
-         * may be thrown when initialising the map
-         */
-
-        try {
-            if (googleMap == null) {
-                googleMap = ((MapFragment) getFragmentManager().findFragmentById(
-                        R.id.mapView)).getMap();
-
-                /**
-                 * If the map is still null after attempted initialisation,
-                 * show an error to the user
-                 */
-                if (null == googleMap) {
-                    Toast.makeText(getApplicationContext(),
-                            "Error creating map", Toast.LENGTH_SHORT).show();
-                }
-            }
-        } catch (NullPointerException exception) {
-            Log.e("mapApp", exception.toString());
-        }
-    }
-//    UiSettings mapSettings;
-//    mapSettings = googleMap.getUiSettings();
-    /**
-     * Adds a marker to the map
-     */
-    private void addMarker() {
-
-        /** Make sure that the map has been initialised **/
-        if (null != googleMap) {
-            googleMap.addMarker(new MarkerOptions()
-                            .position(new LatLng(0, 0))
-                            .title("Marker")
-                            .draggable(true)
-            );
-        }
-    }
-
-    StreetViewPanorama m_StreetView;
-
-    private void createStreetView() {
-        m_StreetView = ((StreetViewPanoramaFragment) getFragmentManager().findFragmentById(R.id.streetView)).getStreetViewPanorama();
-    }
-
- //   Set up the onClickListener that will pass the selected lat/long
-   // co-ordinates through to the Street View fragment for loading
-//    public class MapsActivity extends FragmentActivity implements
-//            GoogleApiClient.ConnectionCallbacks,
-//            GoogleApiClient.OnConnectionFailedListener {
-//        @Override
-//        public void onConnected(Bundle bundle) {
-//            Log.i(TAG, "Location services connected.");
-//        }
-//
-//        @Override
-//        public void onConnectionSuspended(int i) {
-//            Log.i(TAG, "Location services suspended. Please reconnect.");
-//        }
-//
-//        @Override
-//        protected void onPause() {
-//            super.onPause();
-//            if (mGoogleApiClient.isConnected()) {
-//                mGoogleApiClient.disconnect();
-//            }
-//        }
-
-
         @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
+        public boolean onOptionsItemSelected (MenuItem item){
             // Handle action bar item clicks here. The action bar will
             // automatically handle clicks on the Home/Up button, so long
             // as you specify a parent activity in AndroidManifest.xml.
@@ -227,11 +112,8 @@ public class SearchLocation extends ActionBarActivity {
             return super.onOptionsItemSelected(item);
         }
 
-//        @Override
-//        public void onConnectionFailed(ConnectionResult connectionResult) {
-//
-//        }
     }
+
 
 
 
