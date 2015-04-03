1f5df909603194e10957e383a62e4cfd4c644f87
king.cheung <king.cheung@joysprouts.com>
2015-04-03 22:12:54 +0800

    Category list implemented

--- PriceSharingApplication/app/src/main/java/com/example/tszwingyim/pricesharingapplication/sugarlist.java
+++ PriceSharingApplication/app/src/main/java/com/example/tszwingyim/pricesharingapplication/sugarlist.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -58,28 +59,33 @@ public class sugarlist extends ActionBarActivity {
             }
         });
 
+        String category = this.getIntent().getExtras().getString("Category");
+        String[] items = null;
+        if (category.equalsIgnoreCase("sugar")) {
+            items = new String[]{"即食麵 / 快熟粉","烏冬麵","非即食粉麵","調味 / 煮食用料","急凍加工食品 / 冷藏加工食品"};
+        } else if (category.equalsIgnoreCase("bread")) {
+            items = new String[]{"蛋糕1","麵包","穀類早餐","麵包醬","蜂蜜 / 蜜糖 / 糖漿"};
+        }else if (category.equalsIgnoreCase("dairy")) {
+            items = new String[]{"牛奶 / 牛奶飲品","芝士 / 乳酪 / 乳酸產品","牛油 / 植物牛油 / 忌廉","冰凍甜點","豆漿 / 豆奶","豆腐製品","蛋類"};
+        }else if (category.equalsIgnoreCase("candy")) {
+            items = new String[]{"糖果 / 甜品","乾果","能量棒 / 營養棒 / 點心棒","餅乾","小食"};
+        }else if (category.equalsIgnoreCase("rice")) {
+            items = new String[]{"米","食用油","罐頭食品","無菌紙盒包裝食品","新鮮蔬菜","新鮮水果","冰鮮/新鮮肉類","經急凍/解凍處理肉類","急凍肉類","急凍海產"};
+        }else if (category.equalsIgnoreCase("drink")) {
+            items = new String[]{"汽水","咖啡 / 茶包 / 即沖奶茶","果汁類飲品","東方特色飲品","樽裝水 / 運動飲品 / 能量飲品","麥片飲品 / 麥芽飲品 / 朱古力飲品"};
+        }else if (category.equalsIgnoreCase("clean")) {
+            items = new String[]{"洗衣用品","清潔用品","紙品","保鮮紙 / 食物袋 / 錫紙","吸濕防霉","電器","寵物食品"};
+        }else if (category.equalsIgnoreCase("daily")) {
+            items = new String[]{"口腔護理","女士衛生用品","沐浴露 / 皂液 / 肥皂","頭髮護理","皮膚護理","女士脫毛用品 / 除毛用品","男士護理","香體用品","藥品","避孕","成人紙尿片 / 紙尿褲"};
+        }else if (category.equalsIgnoreCase("baby")) {
+            items = new String[]{"嬰幼兒奶粉 / 兒童奶粉","孕婦奶粉","成人奶粉","嬰幼兒食品 / 嬰兒食品 / 幼兒食品","嬰兒用品"};
+        }else if (category.equalsIgnoreCase("alcohol")) {
+            items = new String[]{"啤酒","仙地","紅酒","白酒","紹興酒","米酒"};
+        }
+
         ListView searcharray = (ListView) this.findViewById(R.id.listView2);
-//        Intent myIntent11 = getIntent();
-        String[] itembread = new String[]{"蛋糕1","麵包","穀類早餐","麵包醬","蜂蜜 / 蜜糖 / 糖漿"};
-        String[] itemdairy = new String[]{"牛奶 / 牛奶飲品","芝士 / 乳酪 / 乳酸產品","牛油 / 植物牛油 / 忌廉","冰凍甜點","豆漿 / 豆奶","豆腐製品","蛋類"};
-        String[] itemcandy = new String[]{"糖果 / 甜品","乾果","能量棒 / 營養棒 / 點心棒","餅乾","小食"};
-        String[] itemrice = new String[]{"米","食用油","罐頭食品","無菌紙盒包裝食品","新鮮蔬菜","新鮮水果","冰鮮/新鮮肉類","經急凍/解凍處理肉類","急凍肉類","急凍海產"};
-        String[] itemnoodle = new String[]{"即食麵 / 快熟粉","烏冬麵","非即食粉麵","調味 / 煮食用料","急凍加工食品 / 冷藏加工食品"};
-        String[] itemdrink = new String[]{"汽水","咖啡 / 茶包 / 即沖奶茶","果汁類飲品","東方特色飲品","樽裝水 / 運動飲品 / 能量飲品","麥片飲品 / 麥芽飲品 / 朱古力飲品"};
-        String[] itemdaily = new String[]{"口腔護理","女士衛生用品","沐浴露 / 皂液 / 肥皂","頭髮護理","皮膚護理","女士脫毛用品 / 除毛用品","男士護理","香體用品","藥品","避孕","成人紙尿片 / 紙尿褲"};
-        String[] itemclean = new String[]{"洗衣用品","清潔用品","紙品","保鮮紙 / 食物袋 / 錫紙","吸濕防霉","電器","寵物食品"};
-        String[] itembaby = new String[]{"嬰幼兒奶粉 / 兒童奶粉","孕婦奶粉","成人奶粉","嬰幼兒食品 / 嬰兒食品 / 幼兒食品","嬰兒用品"};
-        String[] itemalcohol = new String[]{"啤酒","仙地","紅酒","白酒","紹興酒","米酒"};
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1,itemnoodle);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1,items);
         searcharray.setAdapter(adapter);
-
-//        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
-//                R.array.searchbread_array, android.R.layout.simple_spinner_item);
-//        ListView myList=
-//                (ListView) findViewById(R.id.listView2);
-//        myList.setAdapter(adapter2);
-
     }
 
     @Override
