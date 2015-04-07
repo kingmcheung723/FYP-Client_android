c1f67c1f0084c996b2200f81e4a595fcbd7fb4c2
King Cheung <kingmcheung723@gmail.com>
2015-04-07 23:19:17 +0800

    Shopping cart list display good id

--- PriceSharingApplication/app/src/main/java/com/example/tszwingyim/pricesharingapplication/ShoppingCart.java
+++ PriceSharingApplication/app/src/main/java/com/example/tszwingyim/pricesharingapplication/ShoppingCart.java
@@ -73,7 +73,9 @@ public class ShoppingCart extends ActionBarActivity {
                         String[] goods = new String[token.countTokens()];
                         int count = 0;
                         while (token.hasMoreTokens()) {
-                            goods[count] = token.nextToken().toString();
+                            String goodId = token.nextToken().toString();
+                            String goodName  = token.nextToken().toString();
+                            goods[count] = goodId + " : " + goodName;
                             count++;
                         }
                         CustomList adapter = new
@@ -85,7 +87,7 @@ public class ShoppingCart extends ActionBarActivity {
                     }
                 }
             };
-            String sql = "SELECT name_zh FROM goods WHERE goods.id IN (SELECT good_id FROM shopping_carts WHERE shopping_carts.member_email = '" + memberEmail + "')";
+            String sql = "SELECT id, name_zh FROM goods WHERE goods.id IN (SELECT good_id FROM shopping_carts WHERE shopping_carts.member_email = '" + memberEmail + "')";
             dbManager.querySql(sql);
         }
 
