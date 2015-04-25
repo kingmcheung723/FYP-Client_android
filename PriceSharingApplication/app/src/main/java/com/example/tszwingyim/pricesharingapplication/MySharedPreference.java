63b0713f78d3773bd301b3fd18de2164976597de
Tsz Wing Yim <natalietw.yim@gmail.com>
2015-04-25 18:15:52 +0800

    Logout with clear member name

--- PriceSharingApplication/app/src/main/java/com/example/tszwingyim/pricesharingapplication/MySharedPreference.java
+++ PriceSharingApplication/app/src/main/java/com/example/tszwingyim/pricesharingapplication/MySharedPreference.java
@@ -4,10 +4,14 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
+
+import java.util.Locale;
 
 class MySharedPreference {
     private static final String MemberName = "MemberName";
     private static final String MyPreferenceName = "MyPreferenceName";
+    private static final String LOCALE = "locale";
 
     public static void saveMemberName(String memberName, Context context) {
         if (memberName != null && memberName.length() > 0) {
@@ -17,6 +21,14 @@ class MySharedPreference {
             // Commit the edits!
             editor.commit();
         }
+    }
+
+    public static void clearMemberName(Context context) {
+        SharedPreferences preference = context.getSharedPreferences(MyPreferenceName, 0);
+        SharedPreferences.Editor editor = preference.edit();
+        editor.remove(MemberName);
+        // Commit the edits!
+        editor.commit();
     }
 
     public static String getMemberName(Context context) {
@@ -36,5 +48,20 @@ class MySharedPreference {
                 })
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .show();
+    }
+
+    public static void setLocale(String localeStr, Context context) {
+        if (localeStr != null && localeStr.length() > 0) {
+            SharedPreferences preference = context.getSharedPreferences(MyPreferenceName, 0);
+            SharedPreferences.Editor editor = preference.edit();
+            editor.putString(LOCALE, localeStr);
+            // Commit the edits!
+            editor.commit();
+        }
+    }
+
+    public static String getLocale(Context context) {
+        SharedPreferences preference = context.getSharedPreferences(MyPreferenceName, 0);
+        return preference.getString(LOCALE, "");
     }
 }
